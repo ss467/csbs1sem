e463907a6641e51a1eacd0d328574aec3c5f60d0
ss467 <72782784+ss467@users.noreply.github.com>
2021-10-24 13:55:01 +0530

    Removed Notification

--- app.js
+++
@@ -1,1 +0,0 @@
-alert("This Website is designed by Srivatsa Joshi CSBS student at JSS STU. This website provides Textbooks, Notes and Many more....Soon Question papers will be uploaded. Stay tuned : ");
